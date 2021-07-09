0dd822f58ff09380d4cd96c54c28bdc2ca10b06d
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-07-09 13:02:42 -0300

    update

---
+++ MRV Developer/Exercises/logical.js
@@ -0,0 +1,9 @@
+let nome = 'Gustavo Henrique'
+let endereco = 'Rua ABC'
+let cep = '002220-010'
+
+console.log(`${nome}\n${endereco}\n${cep}`)
+
+let primeirovalor = parseFloat('10')
+let segundovalor = parseInt('2')
+
