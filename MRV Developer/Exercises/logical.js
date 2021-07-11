71eb3b6b807ed4f29a641346a1c2c5f2290612a8
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-07-11 17:29:45 -0300

    crianaçao do clone netflix

--- MRV Developer/Exercises/logical.js
+++
@@ -1,9 +0,0 @@
-let nome = 'Gustavo Henrique'
-let endereco = 'Rua ABC'
-let cep = '002220-010'
-
-console.log(`${nome}\n${endereco}\n${cep}`)
-
-let primeirovalor = parseFloat('10')
-let segundovalor = parseInt('2')
-
